7365a16eb0d15e874956c97990889d6e5728b019
Andrew VanderVeen <avanderveen@gmail.com>
2011-08-02 16:45:01 -0400

    finished m_pd.h, started on m_pd.c

--- java-libpd/ported-java/Output2.java
+++ java-libpd/ported-java/Output2.java
@@ -1,3 +1,4 @@
+//AT:207
 // Special Comments
 //  - TODO: This should be checked over later, when the code is complete enough that functions
 //      call this or interact with it
@@ -9,9 +10,43 @@ import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class Output2 {
+	// m_imp.h
+
 	// m_pd.h
+	final static int PD_MAJOR_VERSION = 0;
+	final static int PD_MINOR_VERSION = 43;
+	final static int PD_BUGFIX_VERSION = 0;
+	final static String PD_TEST_VERSION = "";
+
+	final static int MAXPDSTRING = 1000;
+	final static int MAXPDARG = 5;
+
+	final static int GP_NONE  = 0;
+	final static int GP_GLIST = 1;
+	final static int GP_ARRAY = 2;
+
+	final static int T_TEXT = 0;
+	final static int T_OBJECT = 1;
+	final static int T_MESSAGE = 2;
+	final static int T_ATOM = 3;
+
+	final static int CLASS_DEFAULT = 0;
+	final static int CLASS_PD = 1;
+	final static int CLASS_GOBJ = 2;
+	final static int CLASS_PATCHABLE = 3;
+	final static int CLASS_NOINLET = 8;
+	final static int CLASS_TYPEMASK = 3;
+
+	final static int MAXLOGSIG = 32;
+	final static int MAXLOGSIZE = (1 << MAXLOGSIG);
+
+	final static int LOGCOSTABSIZE = 9;
+	final static int COSTABSIZE = (1 << LOGCOSTABSIZE);
+
+	final static boolean PD_USE_TE_XPIX = true; //m_pd.h:640
+
 	class t_symbol {
-		char[] s_name;
+		String s_name;
 		Object s_thing;
 		t_symbol s_next;
 	}
@@ -51,6 +86,125 @@ public class Output2 {
 		t_atomtype a_type;
 		t_word a_w;
 	}
+
+	//NOTE: possibly unneeded, see m_pd.h:173, m_pd.h:179
+	class t_gobj {
+		t_pd g_pd; //TODO: add t_pd: *t_class: _class - m_imp.h:31
+		t_gobj g_next;
+	}
+
+	class t_scalar {
+		t_gobj sc_gobj;
+		t_symbol sc_template;
+		t_word[] sc_vec;
+	}
+
+	class t_text {
+		t_gobj te_g;
+		t_binbuf te_binbuf;
+		t_outlet te_outlet;
+		t_inlet te_inlet;
+		short te_xpix;
+		short te_ypix;
+		short te_width;
+		int te_type;
+	}
+
+	public static void SETSEMI(t_atom atom) {
+		atom.a_type = A_SEMI;
+		atom.a_w = new t_word_index();
+		atom.a_w.w_index = 0;
+	}
+
+	public static void SETCOMMA(t_atom atom) {
+		atom.a_type = A_COMMA;
+		atom.a_w = new t_word_index();
+		atom.a_w.w_index = 0;
+	}
+
+	public static void SETPOINTER(t_atom atom, t_gpointer gp) {
+		atom.a_type = A_POINTER;
+		atom.a_w = new t_word_gpointer();
+		atom.a_w.w_gpointer = gp;
+	}
+
+	public static void SETFLOAT(t_atom atom, float f) {
+		atom.a_type = A_FLOAT;
+		atom.a_w = new t_word_float();
+		atom.a_w.w_float = f;
+	}
+
+	public static void SETSYMBOL(t_atom atom, t_symbol s) {
+		atom.a_type = A_SYMBOL;
+		atom.a_w = new t_word_symbol();
+		atom.a_w.w_symbol = s;
+	}
+
+	public static void SETDOLLAR(t_atom atom, int n) {
+		atom.a_type = A_DOLLAR;
+		atom.a_w = new t_word_index();
+		atom.a_w.w_index = n;
+	}
+
+	public static void SETDOLLSYM(t_atom atom, t_symbol s) {
+		atom.a_type = A_DOLLSYM;
+		atom.a_w = new t_word_symbol();
+		atom.a_w.w_symbol = s;
+	}
+
+	class t_signal {
+		int s_n;
+		t_sample s_vec; //TODO: where's t_sample from?
+		t_float s_sr;
+		int s_refcount;
+		int s_isborrowed;
+		t_signal s_borrowedfrom;
+		t_signal s_nextfree;
+		t_signal s_nextused;
+		int s_vecsize;
+	}
+
+	class t_resample {
+		int method;
+		long downsample;
+		long upsample;
+		t_sample s_vec;
+		int s_n;
+		t_sample coeffs;
+		int coefsize;
+		t_sample buffer;
+		int bufsize;
+	}
+
+	// m_pd.c:55
+	public static t_pd pd_new(t_class c) {
+		//NOTE: What? check if t_class, t_pd, t_text, t_object are actually all the same
+		if(c.patchable) {
+			c.ob_inlet = 0;
+			c.ob_outlet = 0;
+		}
+		return c;
+	}
+
+	public static void pd_free(t_pd x) {
+		if(x.c_freemethod)
+			x.c_freemethod.eval(x);
+		/*if(x.c_patchable) {
+			//NOTE: this is probably unneccessary, Java GC should take care of it.
+			// these methods are not implemented
+			outlet_free(x.ob_outlet);
+			inlet_free(x.ob_inlet);
+			binbuf_free(x.ob_binbuf);
+		}*/
+	}
+
+	public static void gobj_save(t_gobj x, t_binbuf b) {
+		if(x.g_pd.c_savefn)
+			x.g_pd.c_savefn.eval(x, b);
+	}
+
+	// t_bindelem
+
 
 	// m_binbuf.c
 	class t_binbuf {
